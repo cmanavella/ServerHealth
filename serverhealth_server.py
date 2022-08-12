08d5a1e37e97182f00f1817a6fe1f3127e68310c
Cristian Manavella <crismanavella@gmail.com>
2022-08-12 17:12:03 -0300

    Mejoro el server con manejo de exepciones y colores

--- serverhealth_server.py
+++ serverhealth_server.py
@@ -1,22 +1,43 @@
 import socket
+import sys
+import os
+from termcolor import colored
 
 #Instancio un objeto para poder trabajar con el Socket
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-#Seteo el puerto de escucha
-server.bind(('', 1992))
+#Seteo las variables de escucha. Ip en blanco es localhost para recibir
+#externas.
+server_host = ''
+server_port = 1992
+
+#Con bind conecto al server. Lo hago por Try para manejar error en la escucha.
+try:
+	server.bind((server_host, server_port))
+except socket.error as mensaje_error:
+	print("Se produjo un error al escuchar por el puerto: ", server_port)
+	print(mensaje_error)
 
 #Defino la cantidad de conexiones entrantes simultáneas. De momento solo una.
 server.listen(1)
 
-print("Servidor a la escucha")
+print("Estado del Servidor [", colored('OK', 'green'), "]")
+print("Puerto de escucha:", colored(server_port, 'cyan'))
 while True:
-	#Instancio un objeto cliente. Esto me premite recibir datos.
-	client, address = server.accept()
-	mensaje = "Hola Mundo"
-	client.send(mensaje.encode())
+	#Capturo la excepcion de cancelacion del teclado para salir del server
+	try:
+		#Instancio un objeto cliente. Esto me premite recibir datos.
+		client, address = server.accept()
+		mensaje = "Hola Mundo"
+		client.send(mensaje.encode())
 
-	#Cierro las instancias de cliente y servidor
-	client.close()
-
-server.close()
+		#Cierro las instancias de cliente y servidor
+		client.close()
+	except KeyboardInterrupt:
+		print(colored('\nServidor apagado.', 'red'))
+		try:
+			sys.exit(0)
+			server.close()
+		except SystemExit:
+			os._exit(0)
+			server.close()
